73de2330715ea0e685df0588f6d71be9a0250ccb
ccppjj1981 <ccppjj1981115@163.com>
2018-10-22 21:37:07 +0800

    a

---
+++ src/main/java/com/mmall/pojo/GetterTest.java
@@ -0,0 +1,11 @@
+package com.mmall.pojo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: macbook
+ * Date: 18/9/19
+ * Time: 下午11:12
+ * Description: No Description
+ */
+public class GetterTest {
+}
---
+++ src/main/java/com/mmall/pojo/SetterTest.java
@@ -0,0 +1,11 @@
+package com.mmall.pojo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: macbook
+ * Date: 18/10/1
+ * Time: 下午10:28
+ * Description: No Description
+ */
+public class SetterTest {
+}
