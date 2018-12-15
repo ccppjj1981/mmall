ae9ab2eebf1368f56048c2536e7b49b9da6779fb
ccppjj1981 <ccppjj1981115@163.com>
2018-12-15 22:22:07 +0800

    cookie expire

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,46 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: macbook
+ * Date: 18/12/15
+ * Time: 下午9:51
+ * Description: No Description
+ */
+public class SessionExpireFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletReques = (HttpServletRequest) request;
+        String loginToken = CookieUtil.readLoginToken(httpServletReques);
+        if (StringUtils.isNotEmpty(loginToken)){
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJsonStr,User.class);
+            if(user != null){
+                RedisPoolUtil.setExpire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
